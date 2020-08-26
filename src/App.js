bcb796afde18deef30b1d6aeb1134467b384c0a8
farisaziz12 <farisaziz12@gmail.com>
2020-08-26 17:14:57 +0200

    feat: basic audio player setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import MusicList from "./components/MusicList";
+import AudioProvider from "./providers/AudioProvider";
+import Player from "../src/components/Player";
 
 function App() {
+  const [song, setSong] = useState(undefined);
+  const [canPlay, setCanPlay] = useState(false);
+  const [loadedmetadata, setLoadedmetadata] = useState({});
+  const [timeUpdate, setTimeUpdate] = useState(undefined);
+
+  useEffect(() => {
+    if (song) {
+      const player = new AudioProvider();
+      const songSubscription = player
+        .playStream(song.url)
+        .subscribe((event) => {
+          const audioObj = event.target;
+
+          switch (event.type) {
+            case "canplay":
+              return setCanPlay(true);
+
+            case "loadedmetadata":
+              return setLoadedmetadata({
+                value: true,
+                data: {
+                  time: player.formatTime(audioObj.duration * 1000, "mm:ss"),
+                  timeSec: audioObj.duration,
+                  mediaType: "mp3",
+                },
+              });
+
+            case "timeupdate":
+              setTimeUpdate({
+                timeSec: audioObj.currentTime,
+                time: player.formatTime(audioObj.currentTime * 1000, "mm:ss"),
+              });
+          }
+        });
+    }
+  }, [song]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <header>
+        <nav
+          style={{
+            backgroundColor: "blue",
+            height: "50px",
+            textAlign: "center",
+          }}
         >
-          Learn React
-        </a>
+          <h1 style={{ color: "white" }}>Audio Player</h1>{" "}
+        </nav>
       </header>
-    </div>
+      <div>
+        <img
+          style={{
+            maxHeight: "150px",
+            maxWidth: "auto",
+            marginRight: "auto",
+            marginLeft: "auto",
+            display: "block",
+          }}
+          src="https://maxcdn.icons8.com/Share/icon/Very_Basic/music1600.png"
+        />
+        <h2 style={{ color: "blue", textAlign: "center" }}>Audio Player</h2>
+      </div>
+      <Player
+        currTime={timeUpdate ? timeUpdate.time : "00:00"}
+        duration={loadedmetadata.data ? loadedmetadata.data.time : "00:00"}
+      />
+      <MusicList song={song} setSong={setSong} />
+    </>
   );
 }
 
---
+++ src/components/MusicList.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import CloudProvider from "../providers/CloudProvider";
+import { Subject } from "rxjs";
+
+export default function MusicList(props) {
+  const [songs, setSongs] = useState([]);
+  const cloud = new CloudProvider();
+  const pickSong$ = new Subject();
+  pickSong$.subscribe((song) => props.setSong(song));
+
+  useEffect(() => {
+    const subscription = cloud.getFiles().subscribe(setSongs);
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  const handleClick = (song) => {
+    pickSong$.next(song);
+  };
+
+  return (
+    <div>
+      <ul>
+        {songs.map((song) => (
+          <li key={song.name}>
+            <button
+              onClick={() => handleClick(song)}
+              style={{
+                padding: "3px 3px 3px",
+                backgroundColor: "lightblue",
+                borderColor: "white",
+                borderRadius: "10px",
+              }}
+            >
+              {song.name}
+            </button>{" "}
+            {props.song && props.song.name === song.name ? (
+              <p>Selected</p>
+            ) : null}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
